d04a50a5e8a8863f2140ed6527b6faa140624378
Jochen Kiemes <jochen@kiemes.de>
2017-11-26 18:10:29 +0800

    Intermediate type UtunSocket is unnecessary => simplified code

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::io::Result;
-use std::os::unix::io::{AsRawFd, RawFd, FromRawFd};
+use std::os::unix::io::FromRawFd;
 use std::net::UdpSocket;
 
 #[cfg(target_os = "macos")]
@@ -7,46 +7,26 @@ extern "C" {
     fn open_utun(num :u64) -> i32;
 }
 
-#[derive(Debug)]
-pub struct UtunSocket {
-    pub sock: UdpSocket,
-    name: String
-}
-
-impl UtunSocket {
-    #[cfg(target_os = "macos")]
-    pub fn new() -> Result<UtunSocket> {
-        for utun_n in 0..255 {
-            let fd = unsafe { open_utun(utun_n) as i32};
-            if fd >= 0 {
-                let name = format!("utun{}",1);
-                let sock = unsafe { UdpSocket::from_raw_fd(fd) };
-                return Ok(UtunSocket { sock, name });
-            }
+#[cfg(target_os = "macos")]
+pub fn get_utun() -> Result<(UdpSocket,String)> {
+    for utun_n in 0..255 {
+        let fd = unsafe { open_utun(utun_n) as i32 };
+        if fd >= 0 {
+            let name = format!("utun{}",1);
+            let sock = unsafe { UdpSocket::from_raw_fd(fd) };
+            return Ok((sock, name));
         }
-        Err(std::io::Error::last_os_error())
     }
-
-    #[cfg(not(target_os = "macos"))]
-    pub fn new() -> Result<UtunSocket> {
-        Err("Can open utun only on macos")
-    }
-
-    pub fn if_name(&self) -> &String {
-        &self.name
-    }
+    Err(std::io::Error::last_os_error())
 }
 
-
-impl AsRawFd for UtunSocket {
-    fn as_raw_fd(&self) -> RawFd {
-        self.sock.as_raw_fd()
-    }
+#[cfg(not(target_os = "macos"))]
+pub fn get_utun() -> Result<(UdpSocket,String)> {
+    Err("Can open utun only on macos")
 }
 
 #[cfg(test)]
 mod tests {
-    use UtunSocket;
     use std::process::Command;
 
     fn get_interfaces() -> String {
@@ -57,15 +37,15 @@ mod tests {
         String::from_utf8_lossy(&output.stdout).into_owned()
     }
     
+    #[cfg(target_os = "macos")]
     #[test]
     fn it_works() {
         let iflist_before = get_interfaces();
-        let res = UtunSocket::new();
+        let res = ::get_utun();
         assert!(res.is_ok());
-        let utun_sock = res.unwrap();
-        let if_name = utun_sock.if_name();
+        let (_utun_sock,if_name) = res.unwrap();
         let iflist_after = get_interfaces();
-        assert!(!iflist_before.contains(if_name));
-        assert!( iflist_after.contains(if_name));
+        assert!(!iflist_before.contains(&if_name));
+        assert!( iflist_after.contains(&if_name));
     }
 }
